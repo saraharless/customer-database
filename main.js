c4762559672e2c9127fa601981af5f9e23d5ca07
saraharless <sara.e.harless@gmail.com>
2017-08-01 15:35:57 -0500

    responsive css added

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const cust = customers.results;
 for (var i = 0; i < cust.length; i++) {
 let template =
 `
+<div class = "row">
 <img src= ${cust[i].picture.large}>
 <div class = "names">${cust[i].name.first} ${cust[i].name.last}</div>
 <div class = "email">${cust[i].email}</div>
@@ -14,7 +15,7 @@ ${cust[i].location.state} ${cust[i].location.postcode}</div>
 <div class = "phone">${cust[i].phone}</div>
 <br>
 <div class = "ssn">${cust[i].id.value}</div>
-
+</div>
 `
 let blerg = document.createElement('div');
 blerg.innerHTML = template;
